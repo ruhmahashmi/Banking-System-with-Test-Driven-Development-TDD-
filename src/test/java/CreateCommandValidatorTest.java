f1d778ef14201cc5146c7385216457b4e0cbab4b
Ruhma Hashmi <ruhmahashmi@hotmail.com>
2025-10-31 22:06:23 -0400

    Test Files

---
+++ src/test/java/CreateCommandValidatorTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CreateCommandValidatorTest {
+
+    private final CreateCommandValidator validator = new CreateCommandValidator();
+
+    @Test
+    void valid_create_checking_returns_true() {
+        assertTrue(validator.validate("create checking 12345678 0.6"));
+    }
+
+    @Test
+    void valid_create_savings_returns_true() {
+        assertTrue(validator.validate("create savings 87654321 1.5"));
+    }
+
+    @Test
+    void valid_create_cd_returns_true() {
+        assertTrue(validator.validate("create cd 11111111 2.0"));
+    }
+
+    @Test
+    void invalid_type_returns_false() {
+        assertFalse(validator.validate("create current 12345678 0.6"));
+    }
+
+    @Test
+    void invalid_id_length_returns_false() {
+        assertFalse(validator.validate("create checking 1234567 0.6"));
+    }
+
+    @Test
+    void invalid_id_format_returns_false() {
+        assertFalse(validator.validate("create checking 12345abc 0.6"));
+    }
+
+    @Test
+    void invalid_apr_negative_returns_false() {
+        assertFalse(validator.validate("create checking 12345678 -1.5"));
+    }
+
+    @Test
+    void invalid_apr_high_returns_false() {
+        assertFalse(validator.validate("create checking 12345678 11.0"));
+    }
+
+    @Test
+    void invalid_apr_non_numeric_returns_false() {
+        assertFalse(validator.validate("create checking 12345678 abc"));
+    }
+
+    @Test
+    void missing_parts_returns_false() {
+        assertFalse(validator.validate("create checking 12345678"));
+    }
+
+    @Test
+    void extra_parts_returns_false() {
+        assertFalse(validator.validate("create checking 12345678 0.6 extra"));
+    }
+
+    @Test
+    void empty_command_returns_false() {
+        assertFalse(validator.validate(""));
+    }
+
+    @Test
+    void null_command_returns_false() {
+        assertFalse(validator.validate(null));
+    }
+
+    @Test
+    void wrong_keyword_returns_false() {
+        assertFalse(validator.validate("makes checking 12345678 0.6"));
+    }
+
+    @Test
+    void leading_spaces_returns_true() {
+        assertTrue(validator.validate(" create checking 12345678 0.6"));
+    }
+
+    @Test
+    void trailing_spaces_returns_true() {
+        assertTrue(validator.validate("create checking 12345678 0.6 "));
+    }
+
+    @Test
+    void multiple_spaces_returns_true() {
+        assertTrue(validator.validate("create  checking  12345678  0.6"));
+    }
+}
---
+++ src/test/java/DepositCommandValidatorTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DepositCommandValidatorTest {
+
+    private final DepositCommandValidator validator = new DepositCommandValidator();
+
+    @Test
+    void valid_deposit_returns_true() {
+        assertTrue(validator.validate("deposit 12345678 100.5"));
+    }
+
+    @Test
+    void invalid_id_length_returns_false() {
+        assertFalse(validator.validate("deposit 1234567 100.5"));
+    }
+
+    @Test
+    void invalid_id_format_returns_false() {
+        assertFalse(validator.validate("deposit 12345abc 100.5"));
+    }
+
+    @Test
+    void invalid_amount_negative_returns_false() {
+        assertFalse(validator.validate("deposit 12345678 -100.5"));
+    }
+
+    @Test
+    void invalid_amount_zero_returns_false() {
+        assertFalse(validator.validate("deposit 12345678 0"));
+    }
+
+    @Test
+    void invalid_amount_non_numeric_returns_false() {
+        assertFalse(validator.validate("deposit 12345678 abc"));
+    }
+
+    @Test
+    void missing_parts_returns_false() {
+        assertFalse(validator.validate("deposit 12345678"));
+    }
+
+    @Test
+    void extra_parts_returns_false() {
+        assertFalse(validator.validate("deposit 12345678 100.5 extra"));
+    }
+
+    @Test
+    void empty_command_returns_false() {
+        assertFalse(validator.validate(""));
+    }
+
+    @Test
+    void null_command_returns_false() {
+        assertFalse(validator.validate(null));
+    }
+
+    @Test
+    void wrong_keyword_returns_false() {
+        assertFalse(validator.validate("deposits 12345678 100.5"));
+    }
+
+    @Test
+    void leading_spaces_returns_true() {
+        assertTrue(validator.validate(" deposit 12345678 100.5"));
+    }
+
+    @Test
+    void trailing_spaces_returns_true() {
+        assertTrue(validator.validate("deposit 12345678 100.5 "));
+    }
+
+    @Test
+    void multiple_spaces_returns_true() {
+        assertTrue(validator.validate("deposit  12345678  100.5"));
+    }
+}
