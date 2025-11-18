a380a59eac09d14479f51a5c5c070086b5e34f26
Ruhma Hashmi <ruhmahashmi@hotmail.com>
2025-11-18 02:09:00 -0500

    Make test compile (minimal change)

--- src/main/java/CommandProcessor.java
+++ src/main/java/CommandProcessor.java
@@ -2,9 +2,11 @@ package banking;
 
 public class CommandProcessor {
     private final Bank bank;
+    private final InvalidCommandStorage storage;
 
-    public CommandProcessor(Bank bank) {
+    public CommandProcessor(Bank bank, InvalidCommandStorage storage) {
         this.bank = bank;
+        this.storage = storage;
     }
 
     public void process(String command) {
